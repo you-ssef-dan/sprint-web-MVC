82164e69360bcd40650397fd7efe6738098dae62
y.danoun <youssefdanoun02@gmail.com>
2025-05-13 17:57:08 +0100

    done

--- src/main/java/net/danoun/patientsapp/repository/PatientRepository.java
+++ src/main/java/net/danoun/patientsapp/repository/PatientRepository.java
@@ -1,7 +1,13 @@
 package net.danoun.patientsapp.repository;
 
 import net.danoun.patientsapp.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContainingIgnoreCase(String keyword, Pageable pageable);
+    Page<Patient> findByNomContainingIgnoreCaseOrPrenomContainsIgnoreCase(String nom, String prenom, Pageable pageable);
 }
--- src/main/java/net/danoun/patientsapp/web/PatientController.java
+++ src/main/java/net/danoun/patientsapp/web/PatientController.java
@@ -4,6 +4,8 @@ package net.danoun.patientsapp.web;
 import net.danoun.patientsapp.entities.Patient;
 import net.danoun.patientsapp.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +19,24 @@ public class PatientController {
     PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
+    public String index(Model model ,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        //Page<Patient> pagePatients = patientRepository.findByNomContainingIgnoreCase(keyword, PageRequest.of(page, size));
+        Page<Patient> pagePatients = patientRepository.findByNomContainingIgnoreCaseOrPrenomContainsIgnoreCase(keyword, keyword, PageRequest.of(page, size));
+
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+
         return "patients";
     }
 
     @GetMapping("deletePatient")
-    public String delete(@RequestParam(name = "id") Long id){
+    public String delete(@RequestParam(name = "id") Long id, int page, String keyword) {
         patientRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 }
